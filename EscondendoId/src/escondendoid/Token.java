6b59734364e8389c180e166960072b15bad8811e
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-08-21 20:07:00 -0300

    add Codido de proteção de token

---
+++ EscondendoId/src/escondendoid/Token.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package escondendoid;
+
+/**
+ *
+ * @author admin.zeus
+ */
+public class Token {
+    String id;
+
+    public Token() {
+        this.id = "Modelo de Id de token";
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
+    
+}
