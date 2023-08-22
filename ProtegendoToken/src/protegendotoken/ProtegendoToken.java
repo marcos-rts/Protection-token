efd6519c9525aeb0b72cf4071f7ec8c3c044f68e
Marcos Alexandre Reis Torquato dos Santos <marcos.alex1002@gmail.com>
2023-08-21 21:04:11 -0300

    update coments proteção ao token

--- ProtegendoToken/src/protegendotoken/ProtegendoToken.java
+++ ProtegendoToken/src/protegendotoken/ProtegendoToken.java
@@ -4,12 +4,16 @@ public class ProtegendoToken {
     
     public static void main(String[] args) {
         
+        // Criando uma instância da classe Token para representar uma chave.
         Token chave = new Token();
         
+        // Obtendo e imprimindo o ID da chave recém-criada.
         System.out.println(chave.getId());
         
+        // Atribuindo o ID da chave a uma variável chamada "idApi".
         String idApi = chave.getId();
         
+        // Imprimindo o ID da chave armazenado na variável "idApi".
         System.out.println(idApi);
     }
     
--- ProtegendoToken/src/protegendotoken/Token.java
+++ ProtegendoToken/src/protegendotoken/Token.java
@@ -1,15 +1,20 @@
 package protegendotoken;
-public class Token {
-    String id;
+public class Token { // Esta é a classe Token, que representa um objeto de token.
 
-    public Token() {
+    String id;// Variável para armazenar o ID do token.
+
+    // Construtor padrão da classe Token.
+    public Token() {       
+        // Ao criar uma instância de Token, inicializamos o ID do token com um valor padrão.
         this.id = "Modelo de Id de token";
     }
     
+    // Método para obter o ID do token.
     public String getId() {
         return id;
     }
 
+    // Método para definir o ID do token.
     public void setId(String id) {
         this.id = id;
     }
